d86cd8c0bf23ce0ec13b16a56b78e84856f0be39
sofibenc <sofianesynchro@yahoo.fr>
2014-11-10 12:27:37 +0100

    another_way changes

--- forever/model/Highlighted_products.php
+++ forever/model/Highlighted_products.php
@@ -3,5 +3,11 @@ class Highlighted_products extends Model{
 	public function search($req){
 	}
 
+		public function search($req){
+	}
+		public function search($req){
+	}
+		public function search($req){
+	}
 
 }
--- forever/model/Products_details.php
+++ forever/model/Products_details.php
@@ -3,5 +3,9 @@ class Products_details extends Model{
 	public function search($req){
 		echo "search"
 	}
+	
+	public function search($req){
+		echo "search"
+	}
 
 }
